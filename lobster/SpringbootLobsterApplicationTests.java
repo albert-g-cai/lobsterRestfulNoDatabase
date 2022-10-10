152cfd9e67982149935e9cf83740e0edbaf0ce69
albert-g-cai <90293187+albert-g-cai@users.noreply.github.com>
2022-10-10 18:26:09 -0400

    Add files via upload

---
+++ lobster/SpringbootLobsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.lobster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLobsterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
